52971d58eb9972922d2f07bf45a954ccadc71f70
Daniel <piendl.daniel@gmail.com>
2021-01-15 10:20:05 +0100

    Merge von Leo

--- src/py_uav_path_planning/obstacle_sensor/obstacle_sensor.py
+++ src/py_uav_path_planning/obstacle_sensor/obstacle_sensor.py
@@ -43,25 +43,18 @@ class ObstacleSensor(object):
 
     # New function to identify angle between obstacle and drone"
     def get_angle_uav_obstacles(self, obstacle):
-        # ToDo @Leo
-        return 0
-        assert isinstance(obstacle, Obstacle)
-        sp = np.array([1, 0, 0])  # vector of the sensor in the UAV coordinate frame
-        uav_quat = np.array([self.uav_pose.pose.orientation.x, self.uav_pose.pose.orientation.y,
-                             self.uav_pose.pose.orientation.z, self.uav_pose.pose.orientation.w])
-        rot_matrix = R.from_quat(uav_quat)  # calculates UAV rotation matrix
-        cu_dv = rot_matrix.dot(sp)  # calculates current direction vector of UAV
-        # Build vector between vector and current obstacle
-        uav_pos = np.array([self.uav_pose.pose.position.x, self.uav_pose.pose.position.y,
-                            self.uav_pose.pose.position.z])
+        sp = np.array([1, 0, 0])
+        r = R.from_quat([self.uav_pose.pose.orientation.x, self.uav_pose.pose.orientation.y,
+                         self.uav_pose.pose.orientation.z, self.uav_pose.pose.orientation.w])
+        cu_dv = r.apply(sp)
+        uav_pos = np.array(
+            [self.uav_pose.pose.position.x, self.uav_pose.pose.position.y, self.uav_pose.pose.position.z])
         obs_pos = np.array(obstacle.pose[:3])
-        dist_vector = np.subtract(uav_pos, obs_pos)
-        # Calculate angle between two vectors
-        dist_vector = dist_vector / np.linalg.norm(dist_vector)
-        cu_dv = cu_dv / np.linalg.norm(cu_dv)
-        angle_uav_obs = np.arccos(np.clip(np.dot(dist_vector, cu_dv), -1.0, 1.0))
-        angle_uav_obs = abs(math.degrees(angle_uav_obs))  # turn angle from radians to degrees and give absolute value
-        print(angle_uav_obs)
+        dist_vector = np.subtract(obs_pos, uav_pos)
+        dist_vector_normalized = dist_vector / np.linalg.norm(dist_vector)
+        cu_dv_normalized = cu_dv / np.linalg.norm(cu_dv)
+        angle_uav_obs = np.arccos(np.clip(np.dot(dist_vector_normalized, cu_dv_normalized), -1.0, 1.0))
+        angle_uav_obs = math.degrees(angle_uav_obs)  # turn angle from radians to degrees and give absolute value
         return angle_uav_obs
 
     def _get_all_obstacles(self, filepath):
@@ -78,7 +71,7 @@ class ObstacleSensor(object):
         self.active_obstacles = obstacleListMsg()
         for obstacle_i in self.all_obstacles:
             if self.get_distance_uav_obstacle(obstacle_i) <= self.range and \
-                    self.get_angle_uav_obstacles(obstacle_i) <= self.angle:
+                    (self.get_angle_uav_obstacles(obstacle_i) <= self.angle or 360 - self.angle <= self.get_angle_uav_obstacles(obstacle_i) <= 360):
                 self.active_obstacles.obstacleList.append(obstacle_i)
         return
 
