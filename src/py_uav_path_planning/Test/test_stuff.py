cde1611d29ee257a3455c185587698fb4b4594c0
leonard <leonard.smits@stud.tu-darmstadt.de>
2021-01-23 12:05:34 +0100

    addition of apf calculation

--- src/py_uav_path_planning/Test/test_stuff.py
+++ src/py_uav_path_planning/Test/test_stuff.py
@@ -1,18 +1,35 @@
-import pandas as pd
+# import pandas as pd
 import numpy as np
-
-hello = np.zeros(shape=(2,2))
-print(hello)
-
-bye = np.array([[0.85, 0.98]])
-print(type(bye))
-
-
-
-you = np.add(hello, bye)
-print(you)
-
-golf = you.astype(np.float32).tolist()
-print(golf)
-
-
+# # #    x  y  z
+# # v = np.array([1, 2, 3])
+# # print(v)
+#
+# v = np.zeros(3)
+# v[0] = 1
+# v[1] = 2
+# v[2] = 3
+#
+#
+# #     x1 x2 x3  y1  y2 y3  z1  z2 z3
+# x = [[1, 2, 4], [3, 9, 5], [5, 6, 7]]
+# x = np.array(x)
+#
+# testtest = x.transpose()
+# difference = testtest-v
+#
+# test = np.linalg.norm(difference, axis=0)
+# print(test)
+#
+# pottest = [1, 2, 3]
+#
+# addition = pottest + test
+# print(addition)
+#
+# vector_test = [0, 4, 5]
+# print(np.linalg.norm(vector_test))
+z = np.array([0.5, 5, 6])
+z = np.ma.array(z, mask=(z==0.5))
+d = 1/(z-0.5)
+print(d)
+e = 5+d[0]
+print(e)
